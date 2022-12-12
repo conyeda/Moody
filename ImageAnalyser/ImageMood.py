96a6a647a7ff1c35056f0e8a7ed9995f9ab98804
Anthony5432 <anthonyisaacchuya@gmail.com>
2022-12-12 10:49:54 +0100

    Changed from mood to valence and energy
    We changed from Image mood to valence and energy

--- ImageAnalyser/ImageMood.py
+++ ImageAnalyser/ImageMood.py
@@ -1,6 +1,6 @@
 from threading import Thread
 from threading import Lock
-from ImageAnalyser import ColorMood
+import ColorMood
 
 from PIL import Image
 import numpy
@@ -12,18 +12,24 @@ class ImageMood:
         self._image_path = image_path
         self._image = None
         self._image_data = None
-        self._mood = None
+        self._valence = None
+        self._energy = None
         self._lock = Lock()
     
     @property
-    def mood(self):
-        return self._mood
+    def valence(self):
+        return self._valence
 
-    def analyse_row(self, row):
-        width  = self._image_data.shape[1]
+    @property
+    def energy(self):
+        return self._energy
+
+
+
+    def analyse_row(self, row, width):
         closer_color = 0
         closer_distance = 0
-        current_distance = 0
+        current_energy = 0
         for j in range(0, width):
             closer_color = 0
             closer_distance = numpy.sum(numpy.power(ColorMood.COLORS[0]["color"] - self._image_data[row][j], [2, 2, 2]))
@@ -34,16 +40,28 @@ class ImageMood:
                     closer_color = c
             # TODO: Race condition. Check if solved
             self._lock.acquire()
-            self._frequencies_array[closer_color] += 1
+            self._valence += ColorMood.COLORS[closer_color]["valence"]
+            self._energy += ColorMood.COLORS[closer_color]["energy"]
             self._lock.release()
 
+
+
     def analyse(self):
-        if not self._mood:
+        if not self._valence:
             self._image = Image.open(self._image_path)
             self._image_data = numpy.asarray(self._image)
-            self._frequencies_array = numpy.zeros(len(ColorMood.COLORS))
             height = self._image_data.shape[0]
+            width = self._image_data.shape[1]
+            self._valence = 0
+            self._energy = 0
             for i in range(0, height):
-                Thread(target=self.analyse_row, args=[i]).run()
+                Thread(target=self.analyse_row, args=[i, width]).run()
                 
-            self._mood = ColorMood.COLORS[numpy.argmax(self._frequencies_array)]["mood"]
+            self._valence /= (height*width)
+            self._energy /= (height*width)
+
+if __name__ == "__main__":
+        image_mood =  ImageMood("C:\\Users\\antho\\OneDrive\\Escritorio\\Moody\\images\\example.jpeg")
+        image_mood.analyse()
+        print(image_mood.valence)
+        print(image_mood.energy)   
